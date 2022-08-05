3f7de8dcfefa3d6ff8d35e4168786de1c0fc3077
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-05 23:56:49 +0300

    Добавляет 1 задачу

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startEl = document.querySelector('button[data-start]');
+const stopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startEl.addEventListener('click', onStartColor);
+stopEl.addEventListener('click', onStopColor); 
+
+function onStartColor() {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startEl.setAttribute('disabled', 'true');
+}
+
+function onStopColor() {
+    clearInterval(timerId);
+    startEl.removeAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
