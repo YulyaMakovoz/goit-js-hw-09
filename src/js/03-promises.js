47c30ee536f36359203697e4198d2ecaee4117c2
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-06 23:58:40 +0300

    Добавляет часть 3 задачи

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,32 @@
+const formEl = document.querySelector('.form');
+const firstDelayEl = document.querySelector('input[name="delay"]');
+const delayStepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
 
+formEl.addEventListener('submit', onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
 
+  let delay = Number(firstDelayEl.value);
+  let step = Number(delayStepEl.value);
+  let amount = Number(amountEl.value);
 
-createPromise(2, 1500)
+  for (let position = 1; position <= amount; position += 1) {
+    delay += step;
+
+    createPromise(position, delay)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+  }
+}
+
+
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
