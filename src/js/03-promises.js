d2101518af5a6c839ac8dfd621d300cc79e51b76
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-07 12:16:11 +0300

    Добавляет 3 задачу

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,6 +5,8 @@ const amountEl = document.querySelector('input[name="amount"]');
 
 formEl.addEventListener('submit', onSubmit);
 
+// Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, сколько ввели в поле amount. При каждом вызове передай ей номер создаваемого промиса (position) и задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
+
 function onSubmit(event) {
   event.preventDefault();
 
@@ -13,8 +15,7 @@ function onSubmit(event) {
   let amount = Number(amountEl.value);
 
   for (let position = 1; position <= amount; position += 1) {
-    delay += step;
-
+    
     createPromise(position, delay)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -22,17 +23,25 @@ function onSubmit(event) {
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+    delay += step;
   }
 }
 
+
+// Дополни код функции createPromise так, чтобы она возвращала один промис, который выполянется или отклоняется через delay времени. Значением промиса должен быть объект, в котором будут свойства position и delay со значениями одноименных параметров. Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
 
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+    } else {
+        reject({ position, delay });
+    }
+    },delay)
+  }) 
+         
 }
